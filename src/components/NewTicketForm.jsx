d65a69602a82cdc3843685d49587004a8d6a0130
Courtney Phillips <courtney@epicodus.com>
2017-08-28 11:13:25 -0700

    Dispatch action in NewTicketForm to update state to include new Ticket created via form.

--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Ticket from "../models/Ticket.js";
 import PropTypes from "prop-types";
 import { connect } from 'react-redux';
+import c from './../constants';
 
 class NewTicketForm extends React.Component {
 
@@ -14,6 +15,16 @@ class NewTicketForm extends React.Component {
   handleNewTicketFormSubmission(event) {
     event.preventDefault()
     const { _names, _location, _issue } = this.refs;
+    const { dispatch } = this.props;
+    const action = {
+      type: c.ADD_TICKET,
+      id: null,
+      names: _names.value,
+      location: _location.value,
+      description: _issue.value,
+      timeOpened: new Date().getTime()
+    }
+    dispatch(action);
     var newTicket = new Ticket(_names.value, _location.value, _issue.value);
     this.props.onNewTicketCreation(newTicket);
     this.props.hideFormAfterSubmission();
