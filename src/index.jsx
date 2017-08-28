3518b059aa0643381b6616e9a0df80439a8d9c31
Courtney Phillips <courtney@epicodus.com>
2017-08-28 10:39:21 -0700

    Create Redux store in existing entry point file.

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from './components/App';
 import styles from './styles/styles.css';
+import { createStore } from 'redux';
+import reducer from './reducers/ticket-list-reducer';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <App />,
