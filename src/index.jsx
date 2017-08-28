f83557b20a6710f864cc770b7ebbb26131c3b53f
Courtney Phillips <courtney@epicodus.com>
2017-08-28 10:40:29 -0700

    Add React-Redux Provider wrapper component as uppermost parent in entry point file. Pass in Redux store.

--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,13 @@ import App from './components/App';
 import styles from './styles/styles.css';
 import { createStore } from 'redux';
 import reducer from './reducers/ticket-list-reducer';
+import { Provider } from 'react-redux';
 
 const store = createStore(reducer);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('react-app-root')
 );
