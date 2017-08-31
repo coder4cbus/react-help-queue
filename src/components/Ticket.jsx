6fb3f8ea75244296940da18fc8ff2c71264ee413
tcsuder <tcsuder@gmail.com>
2017-08-31 16:26:12 -0700

    move timeOpened through to ticket display

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Moment from "moment";
+
 
 function Ticket(props){
+  console.log(Moment);
   return (
     <div>
       <h3>{props.location} - {props.names}</h3>
       <p><em>{props.issue}</em></p>
-      <p>{props.timeSinceOpened}</p>
+      <p>{props.timeOpened}</p>
       <hr/>
     </div>
   );
@@ -15,7 +18,7 @@ function Ticket(props){
 Ticket.propTypes = {
   names: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
-  timeSinceOpened: PropTypes.string.isRequired,
+  timeOpened: PropTypes.number.isRequired,
   issue: PropTypes.string
 };
 
