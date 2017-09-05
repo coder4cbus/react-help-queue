f23dac1db65b5e3182392df95a0a0e4c79fce055
Courtney Phillips <courtney@epicodus.com>
2017-09-05 10:09:07 -0700

    import Redux applyMiddleware() method into entry point file.

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from './components/App';
 import styles from './styles/styles.css';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducers/ticket-list-reducer';
 import { Provider } from 'react-redux';
 
