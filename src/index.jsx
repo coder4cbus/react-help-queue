03aedd7bcb4c7b18c0e88a6dff13e0257fa1505b
Courtney Phillips <courtney@epicodus.com>
2017-09-05 10:39:39 -0700

    Import React-Router's HashRouter into entry point.'

--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,7 @@ import reducer from './reducers/ticket-list-reducer';
 import { Provider } from 'react-redux';
 import middlewareLogger from './middleware/middleware-logger';
 import persistDataLocally from './middleware/persist-local-storage-data';
+import { HashRouter } from 'react-router-dom';
 
 let retrievedState;
 try {
