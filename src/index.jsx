47dcb21181d74c2a97859d82b40bada0dc827393
Courtney Phillips <courtney@epicodus.com>
2017-09-05 10:33:09 -0700

    Add code to initialize Redux store with Tickets saved in localStorage, if available.

--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,17 @@ import { Provider } from 'react-redux';
 import middlewareLogger from './middleware/middleware-logger';
 import persistDataLocally from './middleware/persist-local-storage-data';
 
+let retrievedState;
+try {
+    retrievedState = localStorage.getItem("reduxStore");
+  if (retrievedState === null){
+    retrievedState = [];
+  }
+  retrievedState = JSON.parse(retrievedState);
+} catch (err){
+  retrievedState = [];
+}
+
 const store = createStore(reducer, applyMiddleware(middlewareLogger, persistDataLocally));
 
 ReactDOM.render(
