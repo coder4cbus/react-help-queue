6fd50dc93a660fc76f96702cb421bcdd020340a7
Courtney Phillips <courtney@epicodus.com>
2017-09-05 10:42:05 -0700

    Add two routes to App.jsx.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import Header from './Header';
 import Queue from './Queue';
 import styles from './styles/App.css';
+import { Switch, Route } from 'react-router-dom';
 
 function App(props){
   return (
     <div className = {styles.app}>
       <Header/>
+        <Switch>
+          <Route exact path="/" component={Queue} />
+          <Route path="/admin" component={Admin} />
+        </Switch>
       <Queue/>
     </div>
   );
