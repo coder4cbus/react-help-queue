d58630b7277c8fd2a38df15c90c2317bd7e19cc5
Courtney Phillips <courtney@epicodus.com>
2017-09-06 14:22:55 -0700

    Gather currentRoute from React-Router in Admin component, pass to children as prop.

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -7,8 +7,10 @@ class Admin extends React.Component {
     return(
       <div>
         <h3>Admin Page</h3>
-        <TicketList ticketList = {this.props.adminMasterTicketList}/>
-      </div>      
+        <TicketList
+          ticketList = {this.props.adminMasterTicketList}
+          currentRoute= {this.props.location.pathname}/>
+      </div>
     )
   }
 }
