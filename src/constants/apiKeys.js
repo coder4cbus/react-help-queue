c54e0a1a02cce1cec6db1fae46ab256b7fb27753
Courtney Phillips <courtney@epicodus.com>
2017-09-27 15:07:32 -0700

    Add dummy apiKeys file for demonstration purposes.

---
+++ src/constants/apiKeys.js
@@ -0,0 +1,10 @@
+const firebaseCredentials = {
+  apiKey: "YOUR INFO HERE",
+  authDomain: "YOUR INFO HERE",
+  databaseURL: "YOUR INFO HERE",
+  projectId: "YOUR INFO HERE",
+  storageBucket: "",
+  messagingSenderId: "YOUR INFO HERE"
+};
+
+export default firebaseCredentials;
