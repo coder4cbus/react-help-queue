8b6edd6d2a3aba145a3a4ea63aa7029de964b2a6
Courtney Phillips <courtney@epicodus.com>
2017-09-27 15:02:10 -0700

    Refactor Admin to use Firebase-backed ticket data.

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import TicketList from './TicketList';
 import {connect} from 'react-redux';
 import c from './../constants/constants';
+import { firebase, isLoaded, isEmpty, dataToJS } from 'react-redux-firebase';
 
 class Admin extends React.Component {
 
@@ -20,22 +21,38 @@ class Admin extends React.Component {
   }
 
   render(){
+    const { firebase, firebaseDatabaseObject } = this.props;
+
+    let contentFromFirebase;
+    if (!isLoaded(firebaseDatabaseObject)) {
+      contentFromFirebase = "Loading";
+    } else {
+      if (isEmpty(firebaseDatabaseObject)) {
+        contentFromFirebase = "No tickets in the queue!";
+      } else {
+        let newTicketArray = [];
+        Object.keys(firebaseDatabaseObject).map(key => {
+          newTicketArray.push(firebaseDatabaseObject[key]);
+        })
+        contentFromFirebase = <TicketList
+          ticketList = {newTicketArray}  // Make sure to change this to newTicketArray!
+          currentRoute= {this.props.location.pathname}
+          handleClosingTicket = {this.handleClosingTicket}/>
+      }
+    }
     return(
       <div>
         <h3>Admin Page</h3>
-        <TicketList
-          ticketList = {this.props.adminMasterTicketList}
-          currentRoute= {this.props.location.pathname}
-          handleClosingTicket = {this.handleClosingTicket}/>
+        {contentFromFirebase}
       </div>
     )
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    adminMasterTicketList : state
-  }
-}
+const firebaseWrappedComponent = firebase(['/tickets'])(Admin);
 
-export default connect(mapStateToProps)(Admin);
+export default connect(
+  ({firebase}) => ({
+    firebaseDatabaseObject: dataToJS(firebase, 'tickets')
+  })
+)(firebaseWrappedComponent);
