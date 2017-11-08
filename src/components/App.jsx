4a892f5a2f223339fab382c79b3b423ab5eb041e
Courtney Phillips <courtney@epicodus.com>
2017-11-08 10:59:07 -0800

    Import new Admin component into App component for use in React-Router Switch statement.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import TicketList from './TicketList';
 import Header from './Header';
 import NewTicketControl from './NewTicketControl';
+import Admin from './Admin';
 import { Switch, Route } from 'react-router-dom';
 
 function App(props){
