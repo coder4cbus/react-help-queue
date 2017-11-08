b0fd73379ee6069c8e9294ce1c96149a3b028cff
Lina Shadrach <lina.shadrach@gmail.com>
2017-11-08 13:45:53 -0800

    integrate router to display ticket list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import TicketList from './TicketList';
 import Header from './Header';
-import NewTicketControl from './NewTicketControl';
+import { Switch, Route } from 'react-router-dom';
 
 function App(props){
   return (
     <div>
       <Header/>
-      <TicketList/>
-      <NewTicketControl/>
+      <Switch>
+        <Route exact path="/" component={TicketList} />
+      </Switch>
     </div>
   );
 }
